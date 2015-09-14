a0d8423141dd9ce7885a0f222cc7943f28487872
tbbrave <detong.chang@foxmail.com>
2015-09-14 10:04:25 +0800

    readytotest

---
+++ README.md
@@ -0,0 +1,2 @@
+#3D render
+[reference](http://www.oschina.net/translate/using-docker-as-a-python-development-environment)
--- example_app/app/views.py
+++ example_app/app/views.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import request
+import ctypes
 
-@app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return "Hello, World!"
+
+@app.route('/',methods=['GET','POST'])
+def render():
+    if request.method == 'POST':
+        dll = ctypes.windll.LoadLibrary( 'CloudFunction.dll' )
+        data=request.data
+        result=Cloud_Process(data)
+        return result
+    else:
+        return "hello"
