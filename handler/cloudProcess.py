96b947839df0fd227b158cf44d4c2e1ed6142ce5
tbbrave <detong.chang@foxmail.com>
2015-11-14 10:34:12 +0800

    add socket

--- handler/cloudProcess.py
+++ handler/cloudProcess.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+import socket
 from handler.request import Request
 
 
@@ -7,6 +8,17 @@ class cloudProcess(Request):
         vectors = self.get_argument('vectors')
         precious = self.get_argument('precious')
         headtotail = self.get_argument('headtotail')
-        print vectors
-        print precious
-        print headtotail
+        message = vectors + '|' + precious + '|' + headtotail
+        serverHost = '192.168.0.16'
+        serverPort = '45888'
+        sockobj = socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockobj.connect((serverHost, serverPort))
+        for line in message:
+            # 经过套按字发送line至服务端
+            sockobj.send(line)
+            # 从服务端接收到的数据，上限为1k
+            data = sockobj.recv(1024)
+            # 确认他是引用的，是'x'
+            print 'Client received:', repr(data)
+
+        sockobj.close()
